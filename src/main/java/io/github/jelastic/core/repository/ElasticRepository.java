94bc04af3219dac5f8edebc3549bcdfae244e5e7
sunil.rajashekar <sunil.rsg.dev@gmail.com>
2019-06-27 13:57:11 +0530

    -> BumpUp model and transport client version to 7.1.0 -> CreateMappingRequest takes in MappingSource of type MapElement -> Pass additional field XContentType.JSON when entity is being persisted

--- src/main/java/io/github/jelastic/core/repository/ElasticRepository.java
+++ src/main/java/io/github/jelastic/core/repository/ElasticRepository.java
@@ -47,6 +47,7 @@ import org.elasticsearch.action.update.UpdateRequestBuilder;
 import org.elasticsearch.cluster.metadata.AliasMetaData;
 import org.elasticsearch.cluster.metadata.IndexTemplateMetaData;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
 
@@ -149,7 +150,7 @@ public class ElasticRepository implements Closeable {
                         entitySaveRequest.getMappingType(),
                         entitySaveRequest.getReferenceId()
                 )
-                .setSource(entitySaveRequest.getValue());
+                .setSource(entitySaveRequest.getValue(), XContentType.JSON);
         indexRequestBuilder.setRefreshPolicy(
                 WriteRequest.RefreshPolicy.IMMEDIATE
         ).execute().get();
