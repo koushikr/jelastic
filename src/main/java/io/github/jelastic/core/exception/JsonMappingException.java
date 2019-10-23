eba28c1e26b3c552b236a0000e929e9a0e1cf3d2
sunil.rajashekar <sunil.rajashekar@phonepe.com>
2019-10-23 07:00:10 +0530

    1. Updated JsonTypeInfo property of Filter to 'filterType' instead of 'operator' which doesn't exists 2. Pass additional field XContentType.JSON when updateField is being called -  https://github.com/koushikr/jelastic/pull/2 3. Added JelasticException which extends from Runtime exception and all exception extends JelasticException 4. Added changelog.md file

--- src/main/java/io/github/jelastic/core/exception/JsonMappingException.java
+++ src/main/java/io/github/jelastic/core/exception/JsonMappingException.java
@@ -1,6 +1,6 @@
 package io.github.jelastic.core.exception;
 
-public class JsonMappingException extends RuntimeException {
+public class JsonMappingException extends JelasticException {
 
   public JsonMappingException(String message, Throwable cause) {
     super(message, cause);
